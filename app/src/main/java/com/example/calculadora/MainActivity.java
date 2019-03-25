8699f5828cbbe19d382fcd0e9ce05dd45169319d
monteiro-pro <monteiro_pro@hotmail.com>
2019-03-25 00:35:50 -0300

    Criação do Layout e Iniciado Criaçõ de Métodos de Operação

--- app/src/main/java/com/example/calculadora/MainActivity.java
+++ app/src/main/java/com/example/calculadora/MainActivity.java
@@ -2,12 +2,52 @@ package com.example.calculadora;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView _screen;
+    private String display = "";
+    private String currenteOperatior = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        _screen = (TextView)findViewById(R.id.tvResult);
+        _screen.setText(display);
+    }
+
+    public void updateScreen(){
+        _screen.setText(display);
+    }
+
+    public void onClickNumber(View v){
+        Button b = (Button) v;
+        display += b.getText();
+        updateScreen();
+    }
+
+    public void onClickOperator(View v){
+        Button b = (Button) v;
+        display += b.getText();
+        currenteOperatior = b.getText().toString();
+        updateScreen();
+    }
+
+    private void clear(){
+        display = "";
+        currenteOperatior = "";
+        updateScreen();
+    }
+
+    public void onClickClear(View v){
+        clear();
+        updateScreen();
+    }
+
+    public void onClickEquals(View v){
+
     }
 }
